3536b4940574bf189d2c5dbd08b351961f3502be
kotakota5235 <kotaro5235@gmail.com>
2025-11-20 21:11:09 +0900

    modified fruits.js
    フルーツの大きさを大きくした

--- fruits.js
+++ fruits.js
@@ -1,13 +1,13 @@
 export const FRUITS = [
-    { name: 'cherry', radius: 13, color: '#FF0000', score: 1 },      // Vivid Red
-    { name: 'strawberry', radius: 22, color: '#FF4081', score: 3 },   // Vivid Pink
-    { name: 'grape', radius: 30, color: '#9C27B0', score: 6 },        // Vivid Purple
-    { name: 'dekopon', radius: 36, color: '#FF9800', score: 10 },     // Vivid Orange
-    { name: 'persimmon', radius: 46, color: '#FF5722', score: 15 },   // Deep Vivid Orange
-    { name: 'apple', radius: 58, color: '#D50000', score: 21 },       // Deep Vivid Red
-    { name: 'pear', radius: 66, color: '#FFEB3B', score: 28 },        // Vivid Yellow
-    { name: 'peach', radius: 78, color: '#FF80AB', score: 36 },       // Light Vivid Pink
-    { name: 'pineapple', radius: 90, color: '#FFC107', score: 45 },   // Amber/Gold
-    { name: 'melon', radius: 108, color: '#76FF03', score: 55 },      // Bright Lime Green
-    { name: 'watermelon', radius: 132, color: '#00C853', score: 66 }, // Vivid Green
+    { name: 'cherry', radius: 20, color: '#FF0000', score: 1 },      // Vivid Red
+    { name: 'strawberry', radius: 30, color: '#FF4081', score: 3 },   // Vivid Pink
+    { name: 'grape', radius: 45, color: '#9C27B0', score: 6 },        // Vivid Purple
+    { name: 'dekopon', radius: 55, color: '#FF9800', score: 10 },     // Vivid Orange
+    { name: 'persimmon', radius: 70, color: '#FF5722', score: 15 },   // Deep Vivid Orange
+    { name: 'apple', radius: 85, color: '#D50000', score: 21 },       // Deep Vivid Red
+    { name: 'pear', radius: 100, color: '#FFEB3B', score: 28 },        // Vivid Yellow
+    { name: 'peach', radius: 120, color: '#FF80AB', score: 36 },       // Light Vivid Pink
+    { name: 'pineapple', radius: 140, color: '#FFC107', score: 45 },   // Amber/Gold
+    { name: 'melon', radius: 170, color: '#76FF03', score: 55 },      // Bright Lime Green
+    { name: 'watermelon', radius: 200, color: '#00C853', score: 66 }, // Vivid Green
 ];
